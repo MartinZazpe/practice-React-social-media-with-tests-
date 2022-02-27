6e87cfd49287edddedec4b3223d15a6153ab2ba9
MartinZazpe <martinzazpe1@gmail.com>
2022-02-27 12:23:09 +0100

    changes, more tests added

--- src/pages/SignUp.test.js
+++ src/pages/SignUp.test.js
@@ -53,6 +53,23 @@ describe('Sign up page tests', () => {
             }
         }
 
+        let emailInput, passwordInput, signUpButton
+        const setUpForSubmit = (emailVal = "test@gmail.com", passwordVal = "p4ssw0rd") => {
+            const rendered = render(<SignUp></SignUp>)
+            const { container } = rendered
+
+            emailInput = container.querySelector('#email')
+            fireEvent.change(emailInput, changeEvent(emailVal))
+
+            passwordInput = container.querySelector('#password')
+            fireEvent.change(passwordInput, changeEvent(passwordVal))
+
+            signUpButton = container.querySelector('#btn-register')
+
+            return rendered
+        }
+
+
         test('el input del email tiene el valor del state', () => {
             const { container } = render(<SignUp></SignUp>)
             const emailInput = container.querySelector('#email')
@@ -66,6 +83,31 @@ describe('Sign up page tests', () => {
             fireEvent.change(passwordInput, changeEvent("p4ssword"))
             expect(passwordInput).toHaveValue("p4ssword")
         })
+
+        test('cuando se da click en el boton se sign up y el correo electronico es invalido debe mostrar el error de validacion', () => {
+            const { queryByText } = setUpForSubmit("")
+            fireEvent.click(signUpButton)
+            const error = queryByText('el correo electronico no es valido')
+            expect(error).toBeInTheDocument()
+        })
+
+        test('cuando se da click en el boton se sign up y el correo password es invalido debe mostrar el error de validacion', () => {
+            const { queryByText } = setUpForSubmit("test@gmail.com", "")
+            fireEvent.click(signUpButton)
+            const error = queryByText('La contraseña debe tener entre 6 y 30 caracteres')
+            expect(error).toBeInTheDocument()
+        })
+
+        test('cuando se da click en el boton de sign up con datos correctos, se deben limpiar los errores de validación', () => {
+            const { queryByText } = setUpForSubmit("test@gmail.com", "")
+            fireEvent.click(signUpButton)
+            fireEvent.change(passwordInput, changeEvent('p4ssw0rd'))
+            fireEvent.click(signUpButton)
+            const error = queryByText('La contraseña debe tener entre 6 y 30 caracteres')
+            expect(error).not.toBeInTheDocument()
+        })
+
+
 
     })
 
